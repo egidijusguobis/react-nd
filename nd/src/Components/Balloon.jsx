6e3acea6b242d409640868c9b121dfe29f1d44dd
Egidijus <egidijusx@gmail.com>
2021-10-17 13:28:19 +0300

    poppin baloons

--- nd/src/Components/Balloon.jsx
+++ nd/src/Components/Balloon.jsx
@@ -1,11 +1,36 @@
+import { Component } from 'react';
+
+class Balloon extends Component {
+    constructor() {
+        super()
+        this.state = {
+            color: '#'+Math.floor(Math.random()*16777215).toString(16),
+        };
+
+    }
+
+    click = () => {
+        this.setState({
+            color: 'none',
+        })
+        // console.log('labas');
+    }
 
 
-function Balloon (props) {
-    return (
-        <>
-        <div className='balloon' style={{background: props.color}}></div>
-        </> 
-    );
+    render () {
+        return (
+            <>
+                <div className='balloon' onClick={this.click} style={{background: this.state.color}}></div>
+            </> 
+        )
+    }
 }
 
+
+    //     return (
+//         <>
+//         <div className='balloon' style={{background: props.color}}></div>
+//         </> 
+//     );
+// }
 export default Balloon;
